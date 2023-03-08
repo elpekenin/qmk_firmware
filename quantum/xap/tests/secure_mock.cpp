0123b1569364db1499da9486ae11499ea341adf8
elpekenin <elpekenin@elpekenin.dev>
2023-03-08 11:30:29 +0100

    Headers

--- quantum/xap/tests/secure_mock.cpp
+++ quantum/xap/tests/secure_mock.cpp
@@ -1,4 +1,7 @@
-// No pending (time-based state)
+// Copyright 2023 Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// TODO: SECURE_PENDING (time-based state)
 
 extern "C" {
 #include "secure.h"
--- quantum/xap/tests/xap_mock.cpp
+++ quantum/xap/tests/xap_mock.cpp
@@ -1,3 +1,6 @@
+// Copyright 2023 Pablo Martinez (@elpekenin) <elpekenin@elpekenin.dev>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 // ----------- XAP Mock -----------
 extern "C" {
 #include "xap.h"
