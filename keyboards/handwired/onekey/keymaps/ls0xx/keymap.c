dffaa4b7aec948959d7192459c74eefa1b38fc1a
elpekenin <elpekenin@elpekenin.dev>
2023-07-11 22:23:38 +0200

    nice!view test keymap

--- keyboards/handwired/onekey/keymaps/ls0xx/keymap.c
+++ keyboards/handwired/onekey/keymaps/ls0xx/keymap.c
@@ -12,17 +12,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_ortho_1x1(KC_A)
 };
 
-static painter_device_t       ls013b7dh03;
-static painter_font_handle_t  fira_code;
-static painter_image_handle_t bw_gif;
+static painter_device_t       display;
+static painter_font_handle_t  font;
+static painter_image_handle_t gif;
 
 uint32_t deferred_init(uint32_t trigger_time, void *cb_arg) {
     // qp_animate(ls013b7dh03, 0, 0, bw_gif);
 
     for (uint8_t rotation = 0; rotation < 4; ++rotation) {
-        qp_init(ls013b7dh03, rotation);
-        qp_drawtext(ls013b7dh03, 0, 0, fira_code, "Rotation :)");
-        qp_flush(ls013b7dh03);
+        qp_init(display, rotation);
+        qp_drawtext(display, 0, 0, font, "Rotation :)");
+        qp_flush(display);
         wait_ms(500);
     }
 
@@ -32,10 +32,10 @@ uint32_t deferred_init(uint32_t trigger_time, void *cb_arg) {
 void keyboard_post_init_user(void) {
     debug_enable = true;
 
-    ls013b7dh03 = qp_ls013b7dh03_make_spi_device(CS_PIN, SPI_DIV, 0);
+    display = qp_ls011b7dh03_make_spi_device(CS_PIN, SPI_DIV, 0);
 
-    bw_gif = qp_load_image_mem(gfx_bw);
-    fira_code = qp_load_font_mem(font_fira_code);
+    gif = qp_load_image_mem(gfx_bw);
+    font = qp_load_font_mem(font_fira_code);
 
     defer_exec(3000, deferred_init, NULL);
 }
