cc0f5363102a88e3af6ec2a8b84f19da8151d330
elpekenin <elpekenin@elpekenin.dev>
2025-05-08 16:35:19 +0200

    bitwise

--- quantum/matrix_common.c
+++ quantum/matrix_common.c
@@ -169,7 +169,8 @@ __attribute__((weak)) uint8_t matrix_scan(void) {
     bool changed = matrix_scan_custom(raw_matrix);
 
 #ifdef SPLIT_KEYBOARD
-    changed = debounce(raw_matrix, matrix + thisHand, ROWS_PER_HAND, changed) | matrix_post_scan();
+    changed = debounce(raw_matrix, matrix + thisHand, ROWS_PER_HAND, changed);
+    changed |= matrix_post_scan();
 #else
     changed = debounce(raw_matrix, matrix, ROWS_PER_HAND, changed);
     matrix_scan_kb();
